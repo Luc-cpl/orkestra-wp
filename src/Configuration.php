ddb2c1f73377d3fcd0eb8578e02562383bc5f735
Luc-cpl <lucas.carvalhopl@outlook.com>
2023-10-29 21:20:41 -0300

    fix: wp assets dir

--- src/Configuration.php
+++ src/Configuration.php
@@ -9,7 +9,7 @@ class Configuration extends CoreProxy
 	public function get(string $key): mixed
 	{
 		return match ($key) {
-			'assets' => plugins_url('assets', $this->get('root')),
+			'assets' => plugins_url('public/assets', $this->get('root')),
 			default  => parent::get($key),
 		};
 	}
