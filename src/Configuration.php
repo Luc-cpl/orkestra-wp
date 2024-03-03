86907297fff34ee682153d6170ba454ffce93001
Viktor Szépe <viktor@szepe.net>
2024-03-03 07:31:44 +0100

    Fix infinite loop in Configuration

--- src/Configuration.php
+++ src/Configuration.php
@@ -9,7 +9,7 @@ class Configuration extends CoreProxy
 	public function get(string $key, mixed $default = null): mixed
 	{
 		/** @var string */
-		$root = $this->get('root');
+		$root = parent::get('root');
 		return match ($key) {
 			'assets' => plugins_url('public/assets', $root),
 			default  => parent::get($key),
