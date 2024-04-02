ca9c5c3e0b1ae51dcb3a22d37587f1d2e341c4f2
Lucas Carvalho <lucas.carvalhopl@outlook.com>
2024-04-02 12:15:41 -0300

    add: custom commands handler for maestro

---
+++ src/Providers/CommandsProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OrkestraWP\Providers;
+
+use Orkestra\App;
+use Orkestra\Handlers\CommandsHandler as CoreCommandsHandler;
+use Orkestra\Providers\CommandsProvider as CoreProvider;
+use OrkestraWP\Handlers\CommandsHandler;
+
+class CommandsProvider extends CoreProvider
+{
+	/**
+	 * Register services with the container.
+	 *
+	 * @param App $app
+	 * @return void
+	 */
+	public function register(App $app): void
+	{
+		parent::register($app);
+		$app->bind(CommandsHandler::class, CoreCommandsHandler::class);
+	}
+}
